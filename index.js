d94b215db07bd4385a2e053cacec620d55553949
kusum chaurasia <kusumc>
2026-07-25 20:55:32 +0530

    Day 5: Implemented Express.js backend with GET and POST APIs

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
 const express = require("express");
 
 const app = express();
+const PORT = 3000;
 
+// Middleware
+app.use(express.json());
+
+// Home Route
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("🚀 Blog Management System Backend is Running!");
+    
+
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
+// GET Route
+app.get("/blogs", (req, res) => {
+
+    const blogs = [
+
+        {
+            id: 1,
+            title: "HTML Complete Guide",
+            author: "Kusum"
+        },
+
+        {
+            id: 2,
+            title: "CSS Mastery",
+            author: "Kusum"
+        }
+
+    ];
+
+    res.status(200).json(blogs);
+
+});
+
+// POST Route
+app.post("/blogs", (req, res) => {
+
+    const newBlog = req.body;
+
+    res.status(201).json({
+
+        message: "✅ Blog Added Successfully",
+
+        blog: newBlog
+
+    });
+
+});
+
+// Server
+app.listen(PORT, () => {
+     console.log(`Server is running at http://localhost:${PORT}`);
+     
 });
