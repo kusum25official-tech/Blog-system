c53f6123151ef2316497f7e5348cc275a1b9e181
kusum chaurasia <kusumc>
2026-07-26 20:43:59 +0530

    Day 6: Created Blog API using Express.js and tested with Postman

--- index.js
+++ index.js
@@ -1,36 +1,21 @@
 const express = require("express");
-
 const app = express();
 const PORT = 3000;
 
 // Middleware
 app.use(express.json());
 
+// Store Blogs
+const blogs = [];
+
 // Home Route
 app.get("/", (req, res) => {
     res.send("🚀 Blog Management System Backend is Running!");
     
-
 });
 
-// GET Route
+    // GET Route
 app.get("/blogs", (req, res) => {
-
-    const blogs = [
-
-        {
-            id: 1,
-            title: "HTML Complete Guide",
-            author: "Kusum"
-        },
-
-        {
-            id: 2,
-            title: "CSS Mastery",
-            author: "Kusum"
-        }
-
-    ];
 
     res.status(200).json(blogs);
 
@@ -38,18 +23,23 @@ app.get("/blogs", (req, res) => {
 
 // POST Route
 app.post("/blogs", (req, res) => {
+    const newBlog = {
+        id: blogs.length + 1,
+        title: req.body.title,
+        author: req.body.author,
+        category: req.body.category,
+        content: req.body.content
 
-    const newBlog = req.body;
+    };
 
+    blogs.push(newBlog);
     res.status(201).json({
-
         message: "✅ Blog Added Successfully",
-
         blog: newBlog
-
     });
 
 });
+
 
 // Server
 app.listen(PORT, () => {
